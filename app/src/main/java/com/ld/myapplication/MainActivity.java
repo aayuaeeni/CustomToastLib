a0cd8b0ba00e45dd126d4eac3e00437776c310ae
Aayu <rajusah.aayu@gmail.com>
2019-05-13 12:12:30 +0530

    making method public

--- app/src/main/java/com/ld/myapplication/MainActivity.java
+++ app/src/main/java/com/ld/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.ld.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ld.customtoast.CustomToster;
+
 public class MainActivity extends AppCompatActivity
 {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomToster customToster = new CustomToster();
+//        customToster.showCustomToast();
     }
 }
